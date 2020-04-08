5f9470f6e0939e247bb539087ec6d81a2800e774
aleksandar83p <alex5ko83@gmail.com>
2020-04-08 14:45:13 +0200

    Dodati kontroleri

---
+++ Nekretnine/Controllers/AgentiController.cs
@@ -0,0 +1,44 @@
+﻿using Nekretnine.Models;
+using Nekretnine.Repository.Intefaces;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Nekretnine.Controllers
+{
+    public class AgentiController : ApiController
+    {
+        private IAgentRepository _repository { get; set; }
+
+        public AgentiController(IAgentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Agent> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var agent = _repository.GetById(id);
+            if (agent == null)
+            {
+                return NotFound();
+            }
+            return Ok(agent);
+        }
+
+        [Route("api/najmladji")]
+        public IEnumerable<Agent> GetNajmladji()
+        {
+            return _repository.GetNajmladji();
+        }
+
+        [Route("api/ekstremi")]
+        public IEnumerable<Agent> GetEkstremi()
+        {
+            return _repository.GetEkstremi();
+        }
+    }
+}
---
+++ Nekretnine/Controllers/NekretnineController.cs
@@ -0,0 +1,107 @@
+﻿using Nekretnine.Models;
+using Nekretnine.Repository.Intefaces;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Nekretnine.Controllers
+{
+    public class NekretnineController : ApiController
+    {
+        private INekretninaRepository _repository { get; set; }
+
+        public NekretnineController(INekretninaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Nekretnina> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var nekretnina = _repository.GetById(id);
+            if (nekretnina == null)
+            {
+                return NotFound();
+            }
+            return Ok(nekretnina);
+        }
+
+        public IHttpActionResult Post(Nekretnina nekretnina)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _repository.Add(nekretnina);
+            return CreatedAtRoute("DefaultApi", new { id = nekretnina.Id }, nekretnina);
+        }
+
+        [Authorize]
+        public IHttpActionResult Put(int id, Nekretnina nekretnina)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != nekretnina.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _repository.Update(nekretnina);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return Ok(nekretnina);
+        }
+
+        [Authorize]
+        public IHttpActionResult Delete(int id)
+        {
+            var nekretnina = _repository.GetById(id);
+            if (nekretnina == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(nekretnina);
+            return Ok();
+        }
+
+        public IEnumerable<Nekretnina> GetByNapravljeno(int napravljeno)
+        {
+            return _repository.GetByNapravljeno(napravljeno);
+        }
+
+        [Authorize]
+        [Route("api/pretraga")]
+        public IHttpActionResult PostPretraga(Pretraga pretraga)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (pretraga.Mini > pretraga.Maksi)
+            {
+                return BadRequest("Mini ne sme biti vece od Maksi pretrage");
+            }
+            var nekretnine = _repository.PostPretraga(pretraga.Mini, pretraga.Maksi);
+            if (nekretnine == null)
+            {
+                return NotFound();
+            }
+            return Ok(nekretnine);
+        }
+
+    }
+}
